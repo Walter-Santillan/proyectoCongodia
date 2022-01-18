cb7f29ab5f6827313cb14df9239393b4d46c6bef
Walte® <waltersantillan@disroot.org>
2022-01-18 16:53:48 -0300

    Update supCuadrado.py

--- aprendiendoPython/3) Codificación del diagrama de flujo en Python/supCuadrado.py
+++ aprendiendoPython/3) Codificación del diagrama de flujo en Python/supCuadrado.py
@@ -1,7 +1,11 @@
 #Hallar la superficie de un cuadrado conociendo el valor de un lado.
+print("#################################################")
+print("\tDesarrollado por Best-Seller")
+print("#################################################")
+
 lado = input("Ingresar la medida del lado de un cuadrado: ")
 lado = int(lado)
 
 superficie = lado * lado
-print("La superficie del cuadrado es: ", superficie)
+print("\nLa superficie del cuadrado es: ", superficie)
 #print(superficie)
