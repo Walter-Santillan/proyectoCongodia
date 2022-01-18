d59fa6fde26410d7c6f841bd3715158288b583f6
walter-santillan <waltersantillan@disroot.org>
2022-01-18 11:33:53 -0300

    Cambios realizado, titulogit status -s

--- aprendiendoPython/5) Estructura de Programacion secuencial_1/Ej1.py
+++ aprendiendoPython/5) Estructura de Programacion secuencial_1/Ej1.py
@@ -1,12 +1,18 @@
 #1) Realizar la carga de dos números enteros por teclado e imprimir su suma y su producto.
 
+print("#############################################")
+print("\tDesarrollado por Best-Seller")
+print("#############################################")
+
+print("\nRealizar la carga de dos números")
+
 num1 = int(input("Ingresar el primer numero: "))
 num2 = int(input("Ingresar el segundo numero: "))
 
+#Realizo la operacion
 suma = num1 + num2
 producto = num1 * num2
 
-print("La suma es: ", suma)
-#print(suma)
-print("El producto es: ", producto)
-#print(producto)
+#Imprimo el proyecto
+print("La suma es: ",suma)
+print("El producto es: ",producto)
